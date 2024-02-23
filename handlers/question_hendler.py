79d60e1e4b36e2415a042a28ffd273732ef44c54
Pavel Repin <76727704+IPRepin@users.noreply.github.com>
2024-02-23 10:15:17 +0300

    Fix func question_next and add func any_message

--- handlers/question_hendler.py
+++ handlers/question_hendler.py
@@ -1,6 +1,7 @@
 from aiogram import types, Router, F
 from aiogram.fsm.context import FSMContext
 
+from keyboards.reply_kb import main_keyboard
 from misc.open_quiz import read_quiz_file
 from misc.state import States
 
@@ -8,7 +9,7 @@ router_question = Router()
 
 
 @router_question.message(F.text == "Новый вопрос")
-async def question_next(message: types.Message, state: FSMContext):
+async def question_next(message: types.Message, state: FSMContext) -> None:
     await state.set_state(States.next_questions)
     question_counter = await state.get_data()
     current_value = question_counter.get('current_value', 0)
@@ -16,11 +17,24 @@ async def question_next(message: types.Message, state: FSMContext):
     if current_value < len(questions):
         await state.update_data(current_value=current_value + 1)
         question = questions[current_value]
+        await state.update_data(right_answer=question["Ответ"])
         await message.answer(f'Вопрос:\n{question["Вопрос"]}')
     else:
         await message.reply('Вопросы закончились, вы прошли тест!')
+        await state.clear()
 
 
 @router_question.message(States.next_questions)
 async def any_message(message: types.Message, state: FSMContext) -> None:
-    pass
+    data = await state.get_data()
+    right_answer = data.get('right_answer')
+    await state.update_data(message_answer=message.text)
+    while right_answer == message.text:
+        await message.answer("Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»",
+                             reply_markup=main_keyboard)
+        await state.set_state(States.next_answers)
+        break
+    else:
+        await message.answer("Неправильно. Попробуйте еще раз?",
+                             reply_markup=main_keyboard)
+
