81d384ddeb28c4dace039a8410fac8d728ea029d
Pavel Repin <76727704+IPRepin@users.noreply.github.com>
2024-05-11 12:58:50 +0300

    Update question_hendler.py

--- handlers/question_hendler.py
+++ handlers/question_hendler.py
@@ -20,7 +20,8 @@ async def question_next(message: types.Message, state: FSMContext) -> None:
         await state.update_data(right_answer=question["Ответ"])
         await message.answer(f'Вопрос:\n{question["Вопрос"]}')
     else:
-        await message.reply('Вопросы закончились, вы прошли тест!')
+        await message.answer('Вопросы закончились, вы прошли тест!')
+        await score_reply(message, state)
         await state.clear()
 
 
@@ -35,14 +36,25 @@ async def surrender(message: types.Message, state: FSMContext) -> None:
 
 @router_question.message(States.next_questions)
 async def answer_next_question(message: types.Message, state: FSMContext) -> None:
+    if message.text == "Мой счет":
+        await score_reply(message, state)
     data = await state.get_data()
     right_answer = data.get('right_answer')
-    await state.update_data(message_answer=message.text)
-    while right_answer == message.text:
-        await message.answer("Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»",
+    user_answer = message.text
+    score = data.get("score", 0)  # Получаем значение по умолчанию 0, если нет ключа "score"
+    if right_answer == user_answer:
+        score += 1
+        await state.update_data(score=score)
+        await message.answer("Правильно! Поздравляю! Для следующего вопроса нажмите «Новый вопрос»",
                              reply_markup=main_keyboard)
         await state.set_state(States.next_answers)
-        break
     else:
         await message.answer("Неправильно. Попробуйте еще раз?",
                              reply_markup=main_keyboard)
+
+
+@router_question.message(F.text == "Мой счет")
+async def score_reply(message: types.Message, state: FSMContext) -> None:
+    data = await state.get_data()
+    score = data.get("score", 0)
+    await message.answer(f"Ваш счет: {score}", reply_markup=main_keyboard)
